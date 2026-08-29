f378c7275dc64ead5095c774d0200da37de9efe3
abayduysebayev01-alt <abayduysebayev01@gmail.com>
2026-08-29 12:28:57 +0500

    Add: 10) 1..n ichida 3 ga karralilar soni [abayduysebayev01-alt]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/06-for_va_range_______ketma-ket_takrorlash/10-10__1__n_ichida_3_ga_karralilar_soni.py
@@ -0,0 +1,9 @@
+# n beriladi.
+# for bilan 1..n ichida 3 ga karrali sonlar sonini chiqaring.
+
+n = int(input())
+soni = 0 
+for i in range(1, n + 1):
+    if i % 3 == 0:
+        soni += 1
+print(soni)        
